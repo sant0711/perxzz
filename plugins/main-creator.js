0fbb332b4d23b37b9bd83de0f83d6277407b3752
sant0711 <santx0711@gmail.com>
2024-12-18 13:28:11 -0500

    perxzz

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Perzz 🐱‍�;;\nFN:Perzz 🐱‍�\nORG:Perzz 🐱‍�\nTITLE:\nitem1.TEL;waid=5219612683719:5219612683719\nitem1.X-ABLabel:Perzz 🐱‍�\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Perzz 🐱‍�\nEND:VCARD`
+    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Perzz;;\nFN:Perzz\nORG:Perzz\nTITLE:\nitem1.TEL;waid=5219612683719:5219612683719\nitem1.X-ABLabel:Perzz\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Perzz\nEND:VCARD`
     await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
     }
     handler.help = ['owner']
